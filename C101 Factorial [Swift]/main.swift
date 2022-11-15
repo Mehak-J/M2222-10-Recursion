d95fdc0303e235bb0c2b17860a9ebd21cc1dcb1c
Mehak Jay <mehak.jay@student.allenisd.org>
2022-11-15 08:50:18 -0600

    Recursion

---
+++ C101 Factorial [Swift]/main.swift
@@ -0,0 +1,28 @@
+ 
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func Factorial(n: Int) -> Int {
+    print("Calculating factorial(\(n))...")
+   
+    if n <= 1 {
+        print("Terminal case, returning 1.")
+        return 1
+    } else {
+       
+        let calculation = n * Factorial(n:n - 1)
+        print("Non-terminal case, returning \(calculation).")
+        return calculation
+    }
+}
+
+print("The final result is: \(Factorial(n:n)).")
+
+
---
+++ C102 Fibonacci [Swift]/.merlin/build/main.swift
@@ -0,0 +1,27 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 8
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func fib(n: Int) -> Int {
+    print(" Calculating fibonacci(\(n))...")
+    if (n <= 2) {
+        print("Terminal case, returning 1.")
+        return 1
+    } else {
+        let v = fib(n:n-1) + fib(n:n-2)
+        print("Non-terminal case, returning \(v).")
+        return v
+    }
+}
+
+print("The final result is: \(fib(n:n)).")
+
+
+
